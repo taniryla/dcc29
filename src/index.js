ff323c26bba050a93770dedfd81a8c141cee18ac
Ryland Taniguchi <ryland0000@gmail.com>
2022-03-16 10:52:06 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+/*-----------------------------------------------------------------
+Challenge: 29-addChecker
+Difficulty:  Intermediate
+Prompt:
+- Write a function called addChecker that accepts two arguments.
+- The first argument is an array containing at least two integers.  The integers in the array are sorted in ascending order.
+- The second argument is an integer.
+- The addChecker function should return true if there are two integers in the array of integers (first argument) that when added together, equals the integer passed in as the second argument.
+- If there are no two integers in the array that sum up to equal the second argument, addChecker should return false.
+Hint:
+- An efficient solution can leverage the the fact that the integers in the array are sorted.
+Examples:
+addChecker( [1, 2], 3 ) // => true
+addChecker( [-3, 2], 9 ) // => false
+addChecker( [10, 15, 16, 22], 32 ) // => true
+addChecker( [10, 15, 16, 22], 19 ) // => false
+-----------------------------------------------------------------*/
+// Your solution for 29-addChecker here:
+
+
+</div>
+`;
